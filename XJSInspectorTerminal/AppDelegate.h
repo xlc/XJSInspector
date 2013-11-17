90ab5dde017352ba4e0d05d3313e61bb60114083
Xiliang Chen <xlchen1291@gmail.com>
2013-11-17 18:32:47 +1300

    add XJSInspectorTerminal

---
+++ XJSInspectorTerminal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XJSInspectorTerminal
+//
+//  Created by Xiliang Chen on 13-11-16.
+//  Copyright (c) 2013年 Xiliang Chen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ XJSInspectorTerminal/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  XJSInspectorTerminal
+//
+//  Created by Xiliang Chen on 13-11-16.
+//  Copyright (c) 2013年 Xiliang Chen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
